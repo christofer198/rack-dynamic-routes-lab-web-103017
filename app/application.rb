ddba4715e426e75bfdc93cb6dcf12953f1938c0f
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-20 15:36:47 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,4 +5,16 @@ class Application
     resp = Rack::Response.new
     req = Rack::Response.new(env)
 
-    
+    if req.path.match(/items/)
+
+
+      resp.write Item.items[path]
+
+    else
+      resp.write "404 route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+
+end
