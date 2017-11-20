06a87418f6a3b807a27368f00cb4c9b286fd2562
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-20 15:42:55 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     req = Rack::Response.new(env)
 
     if req.path.match(/items/)
-      item = req.path.split("/items/").last
+      path= req.path.split("/items/").last
 
       if Item.items.keys.include?(path) == true
         resp.write Item.items[path]
