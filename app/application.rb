395e0fa4d08d5ea49af4ec54195377e9a1c84574
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-20 15:44:13 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
 
     resp = Rack::Response.new
-    req = Rack::Response.new(env)
+    req = Rack::Request.new(env)
 
     if req.path.match(/items/)
       uitem = req.path.split("/items/").last
